39216842b372b26070dd4a9a83c1149650b961c2
Peter Maloy <geekbrit@gmail.com>
2013-08-13 08:56:20 -0400

    Added "HereDoc" function prototype extension & made template compilation on-the-fly rather than a separate build step

--- templates/week.template.js
+++ templates/week.template.js
@@ -1,13 +1,18 @@
-/*
+// Multiline Function String - Nate Ferrero - Public Domain
+(function(){}).__proto__.heredoc = function() {
+    return this.toString().replace(/.*\/\*\n([\s\S]*)\n\*\/.*/m, "$1");
+};
+
 //
 //  Template for a calendar for a single resource
 //
+var render_week = doT.template((function(){/*
 <div class="rc_week clearfix">
   <div class="rc_headerweek clearfix">
   <span class='rc_name'>{{=it.resource_name}}</span>
   <span class='rc_location'>{{=it.resource_location}}</span>
   </div>
-  <div class="rc_headerweek"><div class="rc_gutter">&amp;nbsp;</div>
+  <div class="rc_headerweek"><div class="rc_gutter">&nbsp;</div>
   {{ for(var i = 0; i < it.col_count; i++ ) { }}
     <div class="{{=it.col_class}}">{{=it.dows[i]+' '+it.months[i]+'/'+it.days[i]}}</div>
   {{ } }}
@@ -44,19 +49,17 @@
   {{ } }}
   </div>
 </div>
-*/
+*/}).heredoc());
+
+
 
 // Compiled on http://olado.github.io/doT/
 
-var render_week = 
-function anonymous(it /**/) { var out='<div class="rc_week clearfix"> <div class="rc_headerweek clearfix"> <span class=\'rc_name\'>'+(it.resource_name)+'</span> <span class=\'rc_location\'>'+(it.resource_location)+'</span> </div> <div class="rc_headerweek"><div class="rc_gutter">&nbsp;</div> '; for(var i = 0; i < it.col_count; i++ ) { out+=' <div class="'+(it.col_class)+'">'+(it.dows[i]+' '+it.months[i]+'/'+it.days[i])+'</div> '; } out+=' <div class="rc_bodyweek" id=\''+(it.resource_id)+'\'> <div class="rc_gutter"> ';var hrcount =0; var t = it.min_time; do { out+=' <div class=\'rc_hour_slot\'>'+(t)+'</div> '; var r = addMinutes_timeOfDay(t,60,it.max_time); out+=' '; t = r.newtime; hrcount += 1;out+=' '; } while(!r.over);out+=' </div> '; for(var i = 0; i < it.col_count; i++ ) { out+=' <div class="rc_day_target '+(it.col_class)+'" id="'+( it.resource_name+'_'+it.months[i]+'_'+it.days[i] )+'" data-date="'+(it.dates[i])+'"> '; for(var j = 0; j < hrcount; j++ ) { out+=' ';if(it.inc_time == 15){out+=' <div class="rc_quarters_slot"></div> <div class="rc_quarters_slot"></div> <div class="rc_quarters_slot"></div> <div class="rc_quarters_slot"></div> ';}else if(it.inc_time == 20){out+=' <div class="rc_thirds_slot"></div> <div class="rc_thirds_slot"></div> <div class="rc_thirds_slot"></div> ';}else if(it.inc_time == 30){out+=' <div class="rc_halves_slot"></div> <div class="rc_halves_slot"></div> ';}else if(it.inc_time == 60){out+=' <div class="rc_hour_slot"></div> ';}else{out+=' ';}out+=' '; } out+=' </div> '; } out+=' </div></div>';return out; }
 
-
-
-/*
 //
 //  Template for an event in the calendar
 //
+var render_week_event = doT.template((function(){/*
 <div class="rc_event event {{=it.attr.ev_type}}"
      style="top:{{=it.attr.t_offset}}px;height:{{=it.attr.t_height}}px;"
      id="{{=it.attr.id}}">
@@ -65,6 +68,4 @@ function anonymous(it /**/) { var out='<div class="rc_week clearfix"> <div class
   <div class="rc_event_body">{{=it.attr.ev_text}}</div>
   <div class="rc_event_postpad" style="height:{{=it.attr.t_postpad}}px;"></div>
 </div>
-*/
-var render_week_event =
-function anonymous(it /**/) { var out='<div class="rc_event event '+(it.attr.ev_type)+'" style="top:'+(it.attr.t_offset)+'px;height:'+(it.attr.t_height)+'px;" id="'+(it.attr.id)+'"> <div class="rc_event_prepad" style="height:'+(it.attr.t_prepad)+'px;"></div> <div class="rc_event_head">'+(it.attr.start)+' - '+(it.attr.end)+'</div> <div class="rc_event_body">'+(it.attr.ev_text)+'</div> <div class="rc_event_postpad" style="height:'+(it.attr.t_postpad)+'px;"></div></div>';return out; }
+*/}).heredoc());
